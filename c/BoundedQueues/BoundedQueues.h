a2be56b8207701c42b31262e3e10d9db01f4719c
hleppine <hannu.leppinen@gmail.com>
2022-11-27 13:26:25 +0200

    c, BoundedQueues: Add doxygen.cfg, and add comments to BoundedQueues.h

--- c/BoundedQueues/BoundedQueues.h
+++ c/BoundedQueues/BoundedQueues.h
@@ -7,16 +7,30 @@
 #include <stdint.h>
 #include <string.h>
 
+/**
+ * A bounded queue.
+ *  - The queue and item sizes are configured at queue initialization.
+ *  - A buffer is also provided to be used by the queue.
+ *  - Items are memcopied in and out of the queue.
+ */
 typedef struct {
-    size_t maxNbItems;
-    size_t itemNbOctets;
-    size_t nbItems;
-    uint8_t* startPtr;
-    uint8_t* endPtr;
-    uint8_t* frontPtr;
-    uint8_t* backPtr;
+    size_t maxNbItems;   /**< \private */
+    size_t itemNbOctets; /**< \private */
+    size_t nbItems;      /**< \private */
+    uint8_t* startPtr;   /**< \private */
+    uint8_t* endPtr;     /**< \private */
+    uint8_t* frontPtr;   /**< \private */
+    uint8_t* backPtr;    /**< \private */
 } BoundedQueues_Queue_T;
 
+/**
+ * Initializes a bounded queue.
+ * @param queue The queue to initialize.
+ * @param buffer The buffer to use for the queue.
+ * @param bufferNbOctets Size of the buffer in octets.
+ * @param maxNbItems Maximum number of items in the queue.
+ * @param itemNbOctets Size of one item in octets.
+ */
 static inline void BoundedQueues_initialize(BoundedQueues_Queue_T* queue,
                                             uint8_t* buffer,
                                             size_t bufferNbOctets,
@@ -36,22 +50,40 @@ static inline void BoundedQueues_initialize(BoundedQueues_Queue_T* queue,
     };
 }
 
+/**
+ * Returns the current number of items in the queue.
+ * @param queue The queue to access.
+ */
 static inline uint32_t BoundedQueues_nbItems(
     const BoundedQueues_Queue_T* queue) {
     assert(queue != NULL);
     return queue->nbItems;
 }
 
+/**
+ * Returns true if the queue is full, false otherwise.
+ * @param queue The queue to access.
+ */
 static inline bool BoundedQueues_isFull(const BoundedQueues_Queue_T* queue) {
     assert(queue != NULL);
     return queue->nbItems == queue->maxNbItems;
 }
 
+/**
+ * Returns true if the queue is empty, false otherwise.
+ * @param queue The queue to access.
+ */
 static inline bool BoundedQueues_isEmpty(const BoundedQueues_Queue_T* queue) {
     assert(queue != NULL);
     return queue->nbItems == 0UL;
 }
 
+/**
+ * Put an item in the queue.
+ * Returns true if the operation succeeded, and false otherwise.
+ * @param queue The queue to access.
+ * @param item The item to put.
+ */
 static inline bool BoundedQueues_put(BoundedQueues_Queue_T* queue,
                                      const void* item) {
     assert(queue != NULL);
@@ -68,6 +100,12 @@ static inline bool BoundedQueues_put(BoundedQueues_Queue_T* queue,
     return success;
 }
 
+/**
+ * Put an item to the front of the queue.
+ * Returns true if the operation succeeded, and false otherwise.
+ * @param queue The queue to access.
+ * @param item The item to put.
+ */
 static inline bool BoundedQueues_putToFront(BoundedQueues_Queue_T* queue,
                                             const void* item) {
     assert(queue != NULL);
@@ -84,7 +122,13 @@ static inline bool BoundedQueues_putToFront(BoundedQueues_Queue_T* queue,
     return success;
 }
 
-static bool BoundedQueues_get(BoundedQueues_Queue_T* queue, void* item) {
+/**
+ * Get an item from the queue.
+ * Returns true if the operation succeeded, and false otherwise.
+ * @param queue The queue to access.
+ * @param item The obtained item, valid only if true was returned.
+ */
+static inline bool BoundedQueues_get(BoundedQueues_Queue_T* queue, void* item) {
     assert(queue != NULL);
     assert(item != NULL);
     bool success = !BoundedQueues_isEmpty(queue);
@@ -99,8 +143,14 @@ static bool BoundedQueues_get(BoundedQueues_Queue_T* queue, void* item) {
     return success;
 }
 
-static bool BoundedQueues_getFromBack(BoundedQueues_Queue_T* queue,
-                                      void* item) {
+/**
+ * Get an item from the back of the queue.
+ * Returns true if the operation succeeded, and false otherwise.
+ * @param queue The queue to access.
+ * @param item The obtained item, valid only if true was returned.
+ */
+static inline bool BoundedQueues_getFromBack(BoundedQueues_Queue_T* queue,
+                                             void* item) {
     assert(queue != NULL);
     assert(item != NULL);
     bool success = !BoundedQueues_isEmpty(queue);
