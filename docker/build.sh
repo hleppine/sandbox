02cc3a4c429e90742ea198f8931085745dd8b8a2
hleppine <hannu.leppinen@gmail.com>
2023-01-31 19:04:40 +0200

    Add cppcheck to dockerfile, and also to PATH inside docker.
    Can be now called in the docker with just 'cppcheck'

--- docker/build.sh
+++ docker/build.sh
@@ -2,4 +2,18 @@
 
 set -euxo pipefail
 
-docker build --tag dockerproject-image .
+SCRIPT_DIR="$(realpath "$(dirname "$0")")"
+REPO_DIR="${SCRIPT_DIR}/../"
+DEPS_DIR="${SCRIPT_DIR}/deps"
+
+CPPCHECK_VERSION="2.10"
+
+mkdir -p "${DEPS_DIR}"
+
+# Download dependencies if not present.
+pushd "${DEPS_DIR}"
+#wget -nc -O "./cppcheck-${CPPCHECK_VERSION}.tar.gz" "https://github.com/danmar/cppcheck/archive/${CPPCHECK_VERSION}.tar.gz"
+popd
+
+# Build the docker image.
+DOCKER_BUILDKIT=1 docker build --tag dockerproject-image .
