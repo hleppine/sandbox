8d318a699b0ca647fbe240efb2f7d0bc8558a87c
hleppine <hannu.leppinen@gmail.com>
2023-02-03 17:47:55 +0200

    Update build.sh
    Download cppcheck.

--- docker/build.sh
+++ docker/build.sh
@@ -14,7 +14,7 @@ mkdir -p "${DEPS_DIR}"
 
 # Download dependencies if not present.
 pushd "${DEPS_DIR}"
-#wget -nc -O "./cppcheck-${CPPCHECK_VERSION}.tar.gz" "https://github.com/danmar/cppcheck/archive/${CPPCHECK_VERSION}.tar.gz"
+wget -nc -O "./cppcheck-${CPPCHECK_VERSION}.tar.gz" "https://github.com/danmar/cppcheck/archive/${CPPCHECK_VERSION}.tar.gz"
 popd
 
 # Build the docker image.
