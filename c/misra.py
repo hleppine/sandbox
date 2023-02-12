17fd6277afe760f49f446db94137c636219cd25a
hleppine <hannu.leppinen@gmail.com>
2023-02-12 18:09:15 +0200

    Update misra.py

--- c/misra.py
+++ c/misra.py
@@ -8,4 +8,8 @@ script_dir=os.path.dirname(os.path.realpath(__file__))
 for path, dirc, files in os.walk(script_dir):
     for name in files:
         if name.endswith(('.h', '.c')) and name != 'Test.c':
-            print(name)
+            src_path = os.path.join(path, name)
+            dump_path = os.path.join(src_path, '.dump')
+            misra_py = '/usr/lib/x86_64-linux-gnu/cppcheck/addons/misra.py'
+            subprocess.run(['cppcheck', '--dump', src_path])
+            subprocess.run(['python3', misra_py, dump_path])
