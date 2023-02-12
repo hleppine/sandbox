6fc7d827c7310ec2f6b78ef21938802b22fe3db6
hleppine <hannu.leppinen@gmail.com>
2023-02-12 17:50:08 +0200

    Update misra.py

--- c/misra.py
+++ c/misra.py
@@ -1,3 +1,11 @@
 #!/usr/bin/env python3
 
+import os
 import subprocess
+
+script_dir=os.path.dirname(os.path.realpath(__file__))
+
+for path, dirc, files in os.walk(script_dir):
+    for name in files:
+        if name.endswith(('.h', '.c'):
+            print(name)
