5a97f59fb0a0c2fc1df882f37e7f39f91d864a3c
hleppine <hannu.leppinen@gmail.com>
2023-02-14 17:45:10 +0200

    Update Commons.h

--- c/Commons/Commons.h
+++ c/Commons/Commons.h
@@ -3,4 +3,9 @@
 
 #define ARRAY_SIZE(X) (sizeof((X))/sizeof((X)[0]))
 
+static inline uint8_t* Commons_toBytePtr(void* ptr){
+    // No alignment issues possible when using a byte pointer.
+    return (uint8_t*)ptr; // cppcheck-suppress misra-c2012-11.5
+}
+
 #endif /* COMMONS_H */
