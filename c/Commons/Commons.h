adb1226f4241274ee8f78df273680f7683016bb9
hleppine <hannu.leppinen@gmail.com>
2023-02-14 17:55:55 +0200

    Update Commons.h

--- c/Commons/Commons.h
+++ c/Commons/Commons.h
@@ -3,6 +3,8 @@
 
 #define ARRAY_SIZE(X) (sizeof((X))/sizeof((X)[0]))
 
+#define COMMONS__JOIN(A, B) A##B // cppcheck-suppress misra-c2012-20.10
+
 static inline uint8_t* Commons_toBytePtr(void* ptr){
     // No alignment issues possible when using a byte pointer.
     return (uint8_t*)ptr; // cppcheck-suppress misra-c2012-11.5
